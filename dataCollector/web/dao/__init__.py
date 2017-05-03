f5b046930501b53dcee6a885517bfa9922023b64
Jackie9692 <chendaxu2015@gmail.com>
2017-05-03 19:24:30 +0800

    “status”连通数据库

--- dataCollector/web/dao/__init__.py
+++ dataCollector/web/dao/__init__.py
@@ -7,7 +7,7 @@ from sqlalchemy.orm import sessionmaker
 
 Base = declarative_base()
 
-engine = create_engine('mysql://root:admin@localhost:3306/api_validator?charset=utf8', encoding='utf-8', echo=True)
+engine = create_engine('mysql://root:admin@localhost:3306/cmb_sina?charset=utf8', encoding='utf-8', echo=True)
 Base.metadata.create_all(engine)
 
 Session = sessionmaker()
--- dataCollector/web/serviceEntry.py
+++ dataCollector/web/serviceEntry.py
@@ -1,5 +1,10 @@
 # coding:utf-8
 
+import sys
+
+reload(sys)
+sys.setdefaultencoding('utf-8')
+
 import os.path
 
 import tornado.httpserver
@@ -14,40 +19,40 @@ from tornado.options import define, options
 define("port", default=8000, help="run on the given port", type=int)
 
 handlers = [
-    (r"/api/register", handler.RegisterHandler),  #apihandler注册
-    (r"/api/registerSave", handler.RegisterSaveHandler),
-    (r"/api/query", handler.APIQuery),
-    (r"/api/detail", handler.APIDetail),
-    (r"/api/edit", handler.APIEdit),
-    (r"/api/remove", handler.APIRemove),
+    (r"/status/add", handler.StatusHandler)  # 微博写入测试
+    # (r"/api/registerSave", handler.RegisterSaveHandler),
+    # (r"/api/query", handler.APIQuery),
+    # (r"/api/detail", handler.APIDetail),
+    # (r"/api/edit", handler.APIEdit),
+    # (r"/api/remove", handler.APIRemove),
 
-    (r"/class/query", handler.ClassQueryHandler),  #class handler
-    (r"/class/delete", handler.ClassDeleteHandler),
-    (r"/class/create", handler.ClassCreateHandler),
-    (r"/class/edit", handler.ClassEditHandler),
-    (r"/class/editSave", handler.ClassEditSaveHandler),
-
-    (r"/project/query", handler.ProjectQueryHandler),  #类模块handler
-    (r"/project/delete", handler.ProjectDeleteHandler),
-    (r"/project/create", handler.ProjectCreateHandler),
-    (r"/project/edit", handler.ProjectEditHandler),
-    (r"/project/editSave", handler.ProjectEditSaveHandler),
-    (r"/project/detail", handler.ProjectDetailHandler),
-
-    (r"/environment/query", handler.EnvirQueryHandler),  #环境
-    (r"/environment/delete", handler.EnvirDeleteHandler),
-    (r"/environment/create", handler.EnvirCreateHandler),
-    (r"/environment/edit", handler.EnvirEditHandler),
-    (r"/environment/editSave", handler.EnvirEditSaveHandler),
-
-    (r"/module/query", handler.ModuleQueryHandler),  #module
-    (r"/module/delete", handler.ModuleDeleteHandler),
-    (r"/module/create", handler.ModuleCreateHandler),
-    (r"/module/edit", handler.ModuleEditHandler),
-    (r"/module/editSave", handler.ModuleEditSaveHandler),
-
-
-    (r"/test/checkTable", handler.CheckTableHandler)
+    # (r"/class/query", handler.ClassQueryHandler),  #class handler
+    # (r"/class/delete", handler.ClassDeleteHandler),
+    # (r"/class/create", handler.ClassCreateHandler),
+    # (r"/class/edit", handler.ClassEditHandler),
+    # (r"/class/editSave", handler.ClassEditSaveHandler),
+    #
+    # (r"/project/query", handler.ProjectQueryHandler),  #类模块handler
+    # (r"/project/delete", handler.ProjectDeleteHandler),
+    # (r"/project/create", handler.ProjectCreateHandler),
+    # (r"/project/edit", handler.ProjectEditHandler),
+    # (r"/project/editSave", handler.ProjectEditSaveHandler),
+    # (r"/project/detail", handler.ProjectDetailHandler),
+    #
+    # (r"/environment/query", handler.EnvirQueryHandler),  #环境
+    # (r"/environment/delete", handler.EnvirDeleteHandler),
+    # (r"/environment/create", handler.EnvirCreateHandler),
+    # (r"/environment/edit", handler.EnvirEditHandler),
+    # (r"/environment/editSave", handler.EnvirEditSaveHandler),
+    #
+    # (r"/module/query", handler.ModuleQueryHandler),  #module
+    # (r"/module/delete", handler.ModuleDeleteHandler),
+    # (r"/module/create", handler.ModuleCreateHandler),
+    # (r"/module/edit", handler.ModuleEditHandler),
+    # (r"/module/editSave", handler.ModuleEditSaveHandler),
+    #
+    #
+    # (r"/test/checkTable", handler.CheckTableHandler)
 ]
 
 if __name__ == '__main__':
@@ -56,7 +61,7 @@ if __name__ == '__main__':
         handlers=handlers,
         template_path=os.path.join(os.path.dirname(__file__), "template"),
         static_path=os.path.join(os.path.dirname(__file__), "static"),
-        #         debug=True
+        debug=True
     )
     http_server = tornado.httpserver.HTTPServer(app)
     http_server.listen(options.port)
